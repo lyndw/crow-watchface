ac5f120b7208d16657e524f8dd3d0f781694de8d
lyndw <lyndw1@hotmail.com>
2014-12-04 09:25:09 -0800

    Overlay text with image

--- src/main.c
+++ src/main.c
@@ -6,17 +6,48 @@ static GFont s_time_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_crow_bitmap;
 
+static void bitmapLayerUpdate(struct Layer *layer, GContext *ctx){
+  GBitmap *framebuffer;
+  const GBitmap *graphic = bitmap_layer_get_bitmap((BitmapLayer *)layer);
+  int height;
+  uint8_t finalBits;
+  uint8_t *bfr, *bitmap;;
+  
+//  printf("bitmap layer update proc\n");
+  framebuffer = graphics_capture_frame_buffer(ctx);
+  height = graphic->bounds.size.h;
+//  APP_LOG(APP_LOG_LEVEL_DEBUG, "bitmaplayerupdate: %d, height: %d", framebuffer->row_size_bytes, height);
+  
+  for (int yindex =0; yindex < height; yindex++){
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "----- yindex %d", yindex);
+    for ( int xindex = 0; xindex < graphic->row_size_bytes; xindex++){
+      bfr = (((uint8_t*)framebuffer->addr)+(yindex * framebuffer->row_size_bytes)+xindex);
+      bitmap = (((uint8_t*)graphic->addr)+(yindex * graphic->row_size_bytes)+xindex);
+      finalBits = *bitmap ^ *bfr;
+      // APP_LOG(APP_LOG_LEVEL_DEBUG, "bfr: %0x, bitmsp: %0x, finalBits: %x", (unsigned int)bfr, (unsigned int)bitmap, finalBits );
+      
+      *bfr = finalBits;
+    }
+  }
+
+  graphics_release_frame_buffer(ctx, framebuffer);
+  
+}
+// static void dayLayerUpdate(struct Layer *layer, GContext *ctx){
+//  printf("Day layer update proc\n");
+// }
 static void main_window_load(Window *window){
+//  Layer *layer;
     
   // Create GBitmap, then set to created BitmapLayer
   s_crow_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CROW);
-  s_background_layer = bitmap_layer_create(GRect(0, 56, 144, 112));
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_crow_bitmap);
   bitmap_layer_set_background_color(s_background_layer, GColorClear);
 
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 0, 144, 37));
-  text_layer_set_background_color(s_time_layer, GColorClear);
+  s_time_layer = text_layer_create(GRect(0, 38, 144, 37));
+  text_layer_set_background_color(s_time_layer, GColorWhite);
   text_layer_set_text_color(s_time_layer, GColorBlack);
  
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DOS_FONT_36));
@@ -24,19 +55,24 @@ static void main_window_load(Window *window){
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
    // Create day TextLayer
-   s_day_layer = text_layer_create(GRect(0, 30, 144, 62));
-   text_layer_set_background_color(s_day_layer, GColorClear);
+   s_day_layer = text_layer_create(GRect(0, 76, 144, 37));
+   text_layer_set_background_color(s_day_layer, GColorWhite);
    text_layer_set_text_color(s_day_layer, GColorBlack);
  
    text_layer_set_font(s_day_layer, s_time_font);
    text_layer_set_text_alignment(s_day_layer, GTextAlignmentCenter);
 
+// Add it as a child layer to the Window's root layer
 
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_day_layer));
+   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+//   layer = bitmap_layer_get_layer(s_background_layer);
+   layer_set_update_proc(bitmap_layer_get_layer(s_background_layer), bitmapLayerUpdate);
+//   layer_set_update_proc(text_layer_get_layer(s_day_layer), dayLayerUpdate);
 }
+
+
 static void main_window_unload(Window *window){
   fonts_unload_custom_font(s_time_font);
   text_layer_destroy(s_time_layer);
